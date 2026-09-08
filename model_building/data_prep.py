f06876c076d20134c68654da5eefb03d11f40115
Mahalakshmi-Shanmugam <80845130+Mahalakshmi-Shanmugam@users.noreply.github.com>
2026-09-09 01:51:17 +0530

    Update data_prep.py

--- model_building/data_prep.py
+++ model_building/data_prep.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from datasets import Dataset, load_dataset
-
+HF_TOKEN = os.getenv("HF_TOKEN")
 # Load data from HuggingFace
 try:
     dataset = load_dataset("maha5994/tourism-package-prediction", split="train")
